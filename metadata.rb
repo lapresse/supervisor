741ba9c05ae1b97b8d58dc40f7e7433a2e4d8c86
Marc Dufour <kafeolic@gmail.com>
2013-06-27 11:08:39 -0400

    Bump version, now uses platform_family in recipe

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,12 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@opscode.com"
 license           "Apache 2.0"
 description       "Installs supervisor and provides resources to configure services"
-version           "0.4.2"
+version           "0.4.3"
 
 recipe "supervisor", "Installs and configures supervisord"
 
 depends "python"
 
-%w{ ubuntu debian suse redhat }.each do |os|
+%w{ ubuntu debian suse redhat centos }.each do |os|
   supports os
 end
